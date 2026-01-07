33049013f836a104f935748211f5334df386c908
Sarvesh <shindesarvesh727@gmail.com>
2026-01-07 19:39:48 +0530

    🚀 GIGA GLOWUP:  Clerk Webhooks

--- src/db/index.js
+++ src/db/index.js
@@ -2,7 +2,7 @@ import { neon } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-http';
 import * as schema from './schema';
 
-// This uses the DATABASE_URL from your .env.local
+
 const sql = neon(process.env.DATABASE_URL);
 
 // We export the db object with the schema attached for easier queries
